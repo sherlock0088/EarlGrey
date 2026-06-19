dbe77df08d6895bad6462ee2d7c47c87b1a496c6
Sherlock Holmes <sherlockholmes@SherlockMacBook-Pro.local>
2026-06-19 16:55:01 +0800

    Switch to conda EarlGrey and Dfam 3.9

--- Docker/getFiles.sh
+++ Docker/getFiles.sh
@@ -27,7 +27,7 @@ download https://www.repeatmasker.org/rmblast/rmblast-2.13.0+-x64-linux.tar.gz
 download http://eddylab.org/software/hmmer/hmmer-3.3.2.tar.gz
 download https://github.com/Benson-Genomics-Lab/TRF/archive/v4.09.1.tar.gz trf-4.09.1.tar.gz
 download https://www.repeatmasker.org/RepeatScout-1.0.6.tar.gz
-download https://www.repeatmasker.org/RepeatModeler/RECON-1.08.tar.gz
+download https://www.repeatmasker.org/RECON-1.08.tar.gz
 download https://github.com/weizhongli/cdhit/releases/download/V4.8.1/cd-hit-v4.8.1-2019-0228.tar.gz
 download https://github.com/genometools/genometools/archive/v1.6.2.tar.gz gt-1.6.2.tar.gz
 download https://github.com/oushujun/LTR_retriever/archive/v2.9.0.tar.gz LTR_retriever-2.9.0.tar.gz
